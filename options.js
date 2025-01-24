e0f2bf05bda66f840329292cda68710b66adf8b8
the logical guy <Mysstiquee@protonmail.com>
2025-01-24 06:27:32 +0530

    keeping placeholders only for the sake of the example

---
+++ options.js
@@ -0,0 +1,18 @@
+module.exports= [
+    {
+      name: "with options",
+      options: [
+        { name: "Option 1", URL: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" },
+        { name: "Option 2", URL: "https://www.youtube.com/watch?v=D2h4PhgobI8" }
+      ]
+    },
+    {
+      name: "with options 2",
+      options: [
+        { name: "Option 1", URL: "https://www.youtube.com/watch?v=dQw4w9WgXcQ" },
+        { name: "Option 2", URL: "https://www.youtube.com/watch?v=D2h4PhgobI8" },
+        { name: "Option 3", URL: "https://www.youtube.com/watch?v=EE-xtCF3T94" }
+      ]
+    }
+  ];
+  
